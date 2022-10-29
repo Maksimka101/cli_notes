d8327178ea3cae0e645f3861061ca13fa2b66458
Maksim Zemlyanikin <zemlyanikin.maksim@gmail.com>
2022-10-30 01:53:56 +0400

    chor: use dynamic failure

--- src/core/infrastracture/notes_repository/notes_repository.rs
+++ src/core/infrastracture/notes_repository/notes_repository.rs
@@ -1,6 +1,6 @@
 use kv::{Bucket, Config, Integer, Raw, Store};
 
-use crate::core::domain::note::Note;
+use crate::{core::domain::note::Note, DynError, Failure};
 
 pub struct NotesStorage<'a> {
     store: Bucket<'a, Integer, Raw>,
@@ -13,49 +13,38 @@ impl NotesStorage<'_> {
         })
     }
 
-    pub fn save(&self, note: &Note) -> Result<(), Error> {
-        if let Ok(bytes) = note.to_bytes() {
-            if let Ok(_) = self.store.set(&note.id.into(), &Raw::from(bytes)) {
-                return Ok(());
-            }
-        }
-        return Err(Error {});
+    pub fn save(&self, note: &Note) -> Result<(), DynError> {
+        let bytes = note.to_bytes()?;
+        self.store.set(&note.id.into(), &Raw::from(bytes))?;
+        Ok(())
     }
 
-    pub fn read(&self, id: &i32) -> Result<Note, Error> {
+    pub fn read(&self, id: &i32) -> Result<Note, DynError> {
         if let Ok(Some(bytes)) = self.store.get(&id.to_owned().into()) {
             if let Some(note) = Note::from_bytes(&bytes.to_vec()) {
                 return Ok(note);
             }
         }
-        return Err(Error {});
+        return Err(Failure.into());
     }
 
-    pub fn remove(&self, id: &i32) -> Result<Option<Note>, Error> {
+    pub fn remove(&self, id: &i32) -> Result<Option<Note>, DynError> {
         let result = self.store.remove(&id.to_owned().into());
         return match result {
             Ok(Some(bytes)) => Ok(Note::from_bytes(&bytes.to_vec())),
-            _ => Err(Error {}),
+            _ => Err(Failure.into()),
         };
     }
 
-    pub fn read_all(&self) -> Vec<Result<Note, Error>> {
+    pub fn read_all(&self) -> Vec<Result<Note, DynError>> {
         return self
             .store
             .iter()
             .map(|item_result| {
-                if let Ok(item) = item_result {
-                    if let Ok(bytes) = item.value::<Raw>() {
-                        if let Some(note) = Note::from_bytes(&bytes.to_vec()) {
-                            return Ok(note);
-                        }
-                    }
-                }
-
-                return Err(Error {});
+                let bytes = item_result?.value::<Raw>()?;
+                let note = Note::from_bytes(&bytes.to_vec());
+                return note.ok_or_else(|| Failure.into());
             })
             .collect();
     }
 }
-
-pub struct Error {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,19 @@
+use std::{error, fmt};
+
 pub mod cli;
 pub mod core;
+
+/// Dynamic error
+pub type DynError = Box<dyn error::Error>;
+
+/// Implementation of the [DynError].
+#[derive(Debug)]
+pub struct Failure;
+
+impl fmt::Display for Failure {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Generic failure happened")
+    }
+}
+
+impl error::Error for Failure {}
